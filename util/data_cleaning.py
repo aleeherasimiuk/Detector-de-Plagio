697efbc217796723e78e83f5a74ce7d90ce566a2
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2020-10-26 22:22:22 -0300

    Added stemmer and lemmatizer

--- util/data_cleaning.py
+++ util/data_cleaning.py
@@ -1,4 +1,5 @@
 import re
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
 
 def delete_symbols(string):
   symbols = ['\t', '\n', 'l[pic]', '[pic]', '|', '•', '●', '', '\x0c']
@@ -13,12 +14,22 @@ def remove_multiple_whitespaces(string):
 
 
 def separate_glued_words(string):
-    tokens = string.split(' ')
-    for i, token in enumerate(tokens):
-        if not re.search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', token):
-            tokens[i] = tokens[i].replace('.', '. ')
-    return ' '.join(tokens)
+  tokens = string.split(' ')
+  for i, token in enumerate(tokens):
+      if not re.search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', token):
+          tokens[i] = tokens[i].replace('.', '. ')
+  return ' '.join(tokens)
   
 
 def merge_string(splitted_string):
   return ''.join(splitted_string)
+
+
+stemmer = SnowballStemmer('spanish')
+
+def stem_string(string):
+  return stemmer.stem(string)
+
+
+def lemmatize_string(text):
+  return WordNetLemmatizer().lemmatize(text, pos='n')
